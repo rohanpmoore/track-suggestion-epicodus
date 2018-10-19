a7bbdaa42f533db3a4335d9c9936eed4255585e3
Rohan Moore <rohanpmoore@gmail.com>
2018-10-19 10:54:41 -0700

    fix bugs in no suggestion code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-const questionNumber = 5;
+const positiveQuestionNumber = 5;
+/*While it is not used in this program, I added functionality for questions that subtract points from options in order to provide more options should the code be used again in the future*/
+const negativeQuestionNumber = 0;
 const optionNumber = 4;
 
 var determineLargest = function(scores) {
@@ -13,7 +15,7 @@ var determineLargest = function(scores) {
 
 var hasNoResult = function(scores) {
   for (i = 0; i < scores.length; i++) {
-    if (scores[i] >= 0) {
+    if (scores[i] > 0) {
       return false;
     }
   };
@@ -23,28 +25,32 @@ var hasNoResult = function(scores) {
 $(document).ready(function() {
   $("form#quizForm").submit(function(event) {
     event.preventDefault();
-    var answerTally = [0, 0, 0, 0];
+    var answerTally = [];
     var questions = [];
     var answers = []
-    for(i = 0; i < questionNumber; i++) {
+    for(i = 0; i < optionNumber; i++) {
+      answerTally.push(0);
+    }
+    for(i = 0; i < positiveQuestionNumber; i++) {
       questions.push("question" + (i + 1));
     };
-    for(i = 0; i < questionNumber; i++) {
+    for(i = 0; i < positiveQuestionNumber; i++) {
       answers.push(parseInt($("#" + questions[i]).val()));
     };
-    for(i = 0; i < questionNumber; i++) {
+    for(i = 0; i < positiveQuestionNumber; i++) {
       if(!answers[i] && answers[i] != 0) {
         $("#blankForm").show();
         return;
       }
     };
-    for(i = 0; i < questionNumber; i++) {
+    for(i = 0; i < positiveQuestionNumber; i++) {
       if(answers[i] != optionNumber) {
         answerTally[answers[i]]++;
       }
     };
+    debugger;
     var highValue = determineLargest(answerTally);
-    if (noResult) {
+    if (hasNoResult(answerTally)) {
       $("#noSuggestion").show();
     } else if (highValue === 0) {
       $("#ruby").show();
